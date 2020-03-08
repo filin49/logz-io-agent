9e77a4119bfb56da60b20d601cdb544fc9a31a69
Alexandr Dudov <filin49@yandex.ru>
2020-03-09 00:27:19 +0300

    Add gitignore. Fix relative paths. Change messages.

--- monitor.sh
+++ monitor.sh
@@ -8,6 +8,7 @@ if [[ -z "${LOGZ_TOKEN}" ]]; then
 fi
 # Интервал отправки данных
 TIME_INTERVAL=10
+DIRECTORY=$(cd `dirname $0` && pwd)
 
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
@@ -15,24 +16,24 @@ trap "kill 0" EXIT
 echo "Start"
 
 function start_tail() {
-  echo "Запускаю прослушку для $1 в $2.log"
-  tail -Fq -n0 $1 >> ./send/$2.log
+  echo "Start tail $1 to $2.log"
+  tail -Fq -n0 $1 >> $DIRECTORY/send/$2.log
 }
 
 function start_monitor() {
-  echo "Запускаю монитор для $1 в $2.log"
+  echo "Start listen $1 for $2.log"
   file_mask="${1##*/}"
   file_mask="${file_mask/\*/.*}"
   inotifywait -m -r $(dirname "$1") -e create 2> /dev/null |
   while read path action file; do
     if [[ "$path$file" =~ $file_mask ]]; then
-        echo "Новый файл $path$file пишем в $2.log"
-        tail -Fq -n1000 $path$file >> ./send/$2.log
+        echo "New file $path$file stream to $2.log"
+        tail -Fq -n1000 $path$file >> $DIRECTORY/send/$2.log
     fi
   done
 }
 
-find ./conf/ -type f -size +0c |
+find $DIRECTORY/conf/ -type f -size +0c |
   while read file; do
     echo "Read type \"${file##*/}\""
     while IFS= read -r line; do
@@ -48,7 +49,7 @@ find ./conf/ -type f -size +0c |
 
 while :
 do
-  find ./send/ -type f -size +0c |
+  find $DIRECTORY/send/ -type f -size +0c |
     while read file; do
       channel=$(basename "$file" .log)
       echo "Process $file as type $channel"
